c112cac1d5d510183d86f92f4ee06eba76ed9ec1
yeshu2004 <playeryeshu@gmail.com>
2025-05-21 12:53:47 +0530

    try

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,14 @@ import { useEffect } from "react";
 export default function Home() {
 
   useEffect(() => {
+    const setVH = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    setVH();
+    window.addEventListener("resize", setVH);
+
     const lenis = new Lenis({
       autoRaf: true,
     });
@@ -25,13 +33,14 @@ export default function Home() {
     requestAnimationFrame(raf);
     return () => {
       lenis.destroy();
+      window.removeEventListener("resize", setVH);
     };
   }, []);
 
 
   return (
     <div className="">
-      <div className="relative md:h-[100dvh] h-[85dvh] text-white font-clash overflow-hidden bg-[#0F0D18]">
+      <div style={{ height: "calc(var(--vh, 1vh) * 85)" }} className="relative md:h-[100dvh] text-white font-clash overflow-hidden bg-[#0F0D18]">
         <Image
           src="/bgimg.webp"
           alt="Background"
